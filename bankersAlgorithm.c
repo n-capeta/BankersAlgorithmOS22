c2ae1936e0cc268e6849e56f5646dce65426762a
n-capeta <ncapeta@kent.edu>
2022-11-19 12:06:58 -0500

    Setting up matrices and arrays

--- bankersAlgorithm.c
+++ bankersAlgorithm.c
@@ -4,3 +4,25 @@
 
 #include <stdio.h>
 
+    int n, m;
+    n = 5; // Number of processes
+    m = 3; // Number of resources
+
+    // Allocation matrix
+    int alloc[5][3] = { { 0, 1, 0 },
+                        { 2, 0, 0 },
+                        { 3, 0, 2 },
+                        { 2, 1, 1 }, 
+                        { 0, 0, 2 } };
+
+    // Max of matrix
+    int max[5][3] = { { 7, 5, 3 }, 
+                      { 3, 2, 2 }, 
+                      { 9, 0, 2 }, 
+                      { 2, 2, 2 }, 
+                      { 4, 3, 3 } };
+
+    // Resources Available
+    int avail[3] = { 3, 3, 2 };
+
+    
